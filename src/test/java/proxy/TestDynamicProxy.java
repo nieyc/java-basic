1bb0b5f915c9e89ee4556d3955c489046c95dc2c
nieyc <nieyc@cssweb.sh.cn>
2018-04-14 10:39:15 +0800

    测试新电脑

--- src/test/java/proxy/TestDynamicProxy.java
+++ src/test/java/proxy/TestDynamicProxy.java
@@ -10,7 +10,7 @@ public class TestDynamicProxy {
 	@Test
 	public void test() {
 		ProxyHandler proxy = new ProxyHandler();
-        //绑定该类实现的所有接口
+        //绑定该类实现的所有接口。
         Subject sub = (Subject) proxy.bind(new SubjectImpl());
         sub.doSomething();
         sub.sayHello();
